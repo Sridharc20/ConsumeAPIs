87372744cc26e9e7efb1a1fa69e782de25c41bab
Sridharc20 <sridharc20@gmail.com>
2017-02-08 00:26:35 +0530

    new projects

---
+++ REST_API_Project/src/main/java/com/example/Customer.java
@@ -0,0 +1,54 @@
+package com.example;
+
+public class Customer {
+
+	
+private String firstName;
+private String lastName;
+private String place;
+
+
+private Customer() {
+}
+
+
+public Customer(String firstName, String lastName, String place) {
+
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.place = place;
+}
+
+
+public String getFirstName() {
+	return firstName;
+}
+
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+
+public String getLastName() {
+	return lastName;
+}
+
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+
+public String getPlace() {
+	return place;
+}
+
+
+public void setPlace(String place) {
+	this.place = place;
+}
+
+
+
+}
